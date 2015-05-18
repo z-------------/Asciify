b1c8633f76b0786c3b5597adff8de1139ed65b53
Zack Guard <zacharyguard@gmail.com>
2015-05-18 21:18:12 +0800

    First commit

---
+++ demo/demo.js
@@ -0,0 +1,20 @@
+var formInput = document.querySelector("#form_input");
+var outputElem = document.querySelector("#output");
+
+formInput.addEventListener("change", function(){
+    var file = this.files[0];
+    
+    var reader = new FileReader();
+    reader.onload = function() {
+        var dataURI = reader.result;
+        var image = document.createElement("img");
+        image.src = dataURI;
+        image.onload = function(){
+            outputElem.innerHTML = asciify.asciify({ image: image })
+                .replace(/\n/g, "<br>")
+                .replace(/ /g, "&nbsp;");
+        };
+    };
+    
+    reader.readAsDataURL(file);
+});
---
+++ js/asciify.js
@@ -0,0 +1,64 @@
+var asciify = {};
+
+asciify.maps = {
+    "TEN": "@#%*+=-:. ",
+    "FIVE": "@=:. ",
+    "TWO": "@ "
+};
+
+asciify.defaults = {
+    width: 100,
+    map: asciify.maps.TEN,
+    resolutionY: 0.6
+};
+    
+asciify.asciify = function(config) {
+    var options = asciify.defaults;
+    if (config && typeof config === "object" && !Array.isArray(config)) {
+        Object.keys(config).forEach(function(key){
+            options[key] = config[key];
+        });
+    }
+
+    if (options.image) {
+        var map = options.map;
+        var width = options.width;
+        var resolutionY = options.resolutionY;
+        
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+
+        if (options.image instanceof HTMLCanvasElement ||
+            options.image instanceof Image || options.image instanceof HTMLImageElement) {
+            var ratio = options.image.width / options.image.height;
+            canvas.width = width;
+            canvas.height = width / ratio * resolutionY;
+            ctx.drawImage(options.image, 0, 0, canvas.width, canvas.height);
+        } else {
+            return false;
+        }
+
+        var data = ctx.getImageData(0,0,canvas.width,canvas.height).data;
+
+        for (i=0; i<data.length; i+=4)  { // greyscale the image
+            var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+            data[i] = brightness;
+        }
+
+        var asciiPixels = [];
+
+        for (i=0; i<data.length; i+=4) { // do reds only, since it is greyscale
+
+            var rchar = map[Math.round((map.length-1)*data[i]/255)];
+            asciiPixels.push(rchar);
+            
+            if (Math.ceil((i+1)/4) % width == 0) {
+                asciiPixels.push("\n");
+            }
+        }
+
+        return asciiPixels.join("");
+    } else {
+        return false;
+    }
+};
