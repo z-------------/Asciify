0f002ba7c645feca6fc95ee1de458f1d480b5f97
Zachary Guard <zacharyguard@gmail.com>
2018-02-08 10:56:16 +0800

    Only expose asciify() method

--- js/asciify.js
+++ js/asciify.js
@@ -5,41 +5,39 @@ var Asciify = function(image, overrides) {
     image instanceof HTMLImageElement || image instanceof HTMLVideoElement
     )
   ) {
-    this.options = Asciify.defaults;
+    let options = Asciify.defaults;
     if (overrides) {
       let overridesKeys = Object.keys(overrides);
       for (let i = 0, l = overridesKeys.length; i < l; i++) {
-        if (this.options.hasOwnProperty(overridesKeys[i])) {
-          this.options[overridesKeys[i]] = overrides[overridesKeys[i]];
+        if (options.hasOwnProperty(overridesKeys[i])) {
+          options[overridesKeys[i]] = overrides[overridesKeys[i]];
         }
       }
     }
 
-    this.image = image;
-    this.canvas = document.createElement("canvas");
-    this.context = this.canvas.getContext("2d");
-    this.ratio = this.image.width / this.image.height;
+    image = image;
+    canvas = document.createElement("canvas");
+    context = canvas.getContext("2d");
+    ratio = image.width / image.height;
 
-    this.canvas.width = this.options.width;
-    this.canvas.height = this.options.width / this.ratio * this.options.resolutionY;
-
-    console.log(this)
+    canvas.width = options.width;
+    canvas.height = options.width / ratio * options.resolutionY;
 
     this.asciify = function() {
-      this.context.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height);
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
 
-      let data = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height).data;
+      let data = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
       let asciiPixels = "";
 
       for (let i = 0, l = data.length; i < l; i += 4) { // do reds only, since it will be greyscaled
-        asciiPixels += this.options.map[
+        asciiPixels += options.map[
           Math.round(
-            (this.options.map.length - 1) * (0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2]) / 255
+            (options.map.length - 1) * (0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2]) / 255
           )
         ];
 
-        if (Math.ceil((i + 1) / 4) % this.canvas.width === 0) {
+        if (Math.ceil((i + 1) / 4) % canvas.width === 0) {
           asciiPixels += "\n";
         }
       }
